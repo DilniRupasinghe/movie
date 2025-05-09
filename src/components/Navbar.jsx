bf262ac23a9447c5a1b3b94c89764cd973112b5c
DilniRupasinghe <dilni.20230180@iit.ac.lk>
2025-05-09 12:54:37 +0530

    nav bar created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,27 @@
+
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+import { Box } from '@mui/material';
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+          Movie Explorer
+        </Typography>
+        
+        <Box sx={{ display: 'flex', gap: 2 }}>
+          <Button color="inherit" component={Link} to="/">Home</Button>
+          <Button color="inherit" component={Link} to="/favorites">Favorites</Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Navbar;
